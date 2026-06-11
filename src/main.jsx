3937a28fc40f5e6d231293336b180ff3b11b1695
Sergio Jiménez <sjr.social@gmail.com>
2026-06-11 14:25:44 +0200

    fix: update @vercel/analytics import to use the react package

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 import App from './App.jsx';
 
